db64da2b5d8cbfe18be07ef65fd1eb66ebc1c1e5
Vivek Malva <vmalva@sapient.com>
2018-05-22 12:14:39 +0530

    day5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+// import Radium,{StyleRoot} from 'radium';
 import Person from './Person/Person';
 class App extends Component {
 
@@ -56,7 +56,11 @@ class App extends Component {
       font:'inherit',
       border:'1px solid blue',
       padding:'8px',
-      cursor:'pointer'
+      cursor:'pointer',
+      ':hover':{
+        backgroundColor:'lightgreen',
+        color:'black'
+      }
     };
     let persons = null;
     if(this.state.showPersons){
@@ -74,6 +78,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor="red";
+      style[':hover']={
+        backgroundColor:'salmon',
+        color:'black'
+      }
     }
     const classes = [];
     if(this.state.persons.length <=2){
@@ -84,6 +92,7 @@ class App extends Component {
     }
 
     return (
+  
       <div className="App">
         <h1>Hi,I'm a React App</h1>
         <p className={classes.join(" ")}>This better work</p>
@@ -98,4 +107,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './Person.css'
+// import Radium from 'radium';
 
 const person = (props)=>{
+    // const style = {
+    //     '@media (min-width:500px)':{
+    //         width:'450px'
+    //     }
+    // };
     return (
     <div className="Person">
     <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
